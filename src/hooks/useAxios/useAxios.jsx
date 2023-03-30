ff3ff9125ec6590842a9cf950c9a5e663726ffb0
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2023-03-31 00:22:54 +0900

    .

---
+++ src/hooks/useAxios/useAxios.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+const { REACT_APP_MAIN_URL } = process.env;
+
+export const useAxios = () => {
+  return async ({ url, method = "GET", body, headers }) => {
+    return await axios({
+      url: `${REACT_APP_MAIN_URL}${url}`,
+      method: method,
+      data: {
+        ...body,
+      },
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+        "Allow-Control-Origin": true,
+        ...headers,
+      },
+    })
+      .then((response) => response)
+      .catch((error) => error);
+  };
+};
