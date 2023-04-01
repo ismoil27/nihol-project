8200919cf9af21d2e378a6e69a2d7c9459f4f938
Ismoiljon (Jon) <abduraimovismoil98@gmail.com>
2023-04-02 03:51:15 +0900

    Main Cards adding

--- src/Generic/Styles/index.js
+++ src/Generic/Styles/index.js
@@ -5,3 +5,48 @@ export const DropDownContentWrapper = styled.div`
   display: flex;
   grid-gap: 10px;
 `;
+
+export const Title = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 34px;
+  line-height: 77px;
+  color: #000000;
+  margin: 40px;
+  @media (max-width: 600px) {
+    margin: 40px;
+    font-size: 24px;
+  }
+  @media (max-width: 400px) {
+    margin: 40px;
+    font-size: 18px;
+  }
+`;
+
+export const SectionCardContainer = styled.div`
+  margin: 30px auto;
+  width: fit-content;
+  display: flex;
+  grid-gap: 150px;
+  @media (max-width: 800px) {
+    grid-gap: 75px;
+  }
+  @media (max-width: 500px) {
+    grid-gap: 37.5px;
+  }
+  @media (max-width: 270px) {
+    flex-direction: column;
+    grid-gap: 10px;
+    margin: 10px auto;
+  }
+`;
+
+export const CenteredWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Card from "../../Generic/Card";
 import { Wrapper } from "./style";
+import { Title } from "../../Generic/Styles";
+
+import start_time from "../../assets/icons/start_time.svg";
+import end_time from "../../assets/icons/end_time.svg";
+import half_time from "../../assets/icons/half_time.svg";
+import empty_place from "../../assets/icons/empty_place.svg";
+import all_users from "../../assets/icons/all_users.svg";
 
 const Home = () => {
   return (
-    <div>
-      <Wrapper.Title>Bo'limlar</Wrapper.Title>
-      <Card />
-      <Card />
-      <Card />
-    </div>
+    <Wrapper>
+      <Title>Bo'limlar</Title>
+      <Card title={"Barcha mijozlar"} image={all_users} />
+      <Card title={"Oraliq muddat"} image={half_time} />
+      <Card title={"Muddat tugash"} image={end_time} />
+      <Card title={"Mavjud joylar"} image={empty_place} />
+    </Wrapper>
   );
 };
 
